8462bf57d0ceb9f45f4c6d72e9b386a4f7062503
Michael J. Wiebe <wiebe.michael.j@gmail.com>
2017-05-01 16:10:15 -0400

    results not loading, buttons set variable

--- scripts.js
+++ scripts.js
@@ -1,58 +1,41 @@
-// Yelp API Assignment
 
-// Create a site with a search bar and a list of Philadelphia neighborhoods and/or towns and cities in the metro region. These can be listed in a dropdown menu or just as a collection of buttons/divs. There should also be a button or div that when clicked runs a function that: 
-
-// -  Searches the Yelp API for businesses in the selected neighborhood that match the search criteria from the search bar. 
-// - Lists the results on the page with some of the data associated with each result (name, address, image, etc.)
-
-// Additionally: 
 
 // - When a result is clicked, it opens the Yelp page for that business in a new window.
 // - When a user makes a new search, the old results go away and the new results  populate the page and are also clickable.
 
-
-
-
-// $.ajax({
-//     url: "https://api.yelp.com/oauth2/token",
-//     method: "POST",
-//     data:{
-//         client_id: "YJIsPhVU22xhqXbt4Ti3eQ",
-//         client_secret: "rfgBhLQOJRqzpfY0zwVhR0BlprkfdOEvUVj60csXkzo5xhGMZT18HM4yGvzNqsni"
-//     },
-//     dataType:"json",
-//     success: function(response){
-//         console.log(response)
-//     }
-
-// })
-
-    // $.ajax({
-    //     url: "https://api.yelp.com/v3/businesses/search",
-    //     data:{
-    //        term: "pizza",
-    //        location: "Philadelphia",
-    //        radius: 1000
-    //     },
-    //     headers: {
-    //     "Authorization": "Bearer Y7h2GxIY4X2kM8jefOht3EL4zwHefHSPloABF6Ar4F1gzqaWzUu4mpTZiFDcir2sJHcW7meS0brXA5lLjWviOQ9BUH5jpdmVpDKRDy5JZk8zb7ZFWVfDiSult28DWXYx"
-    // },
-    //     dataType:"json",
-    //     success: function(response){
-    //         console.log(response)
-    //     }
-
-    // })
-
-
 var submit = document.getElementsByClassName("submit")[0];
 var search = document.getElementsByClassName("search-box")[0];
 var results = document.getElementsByClassName("results")[0];
+var westPhilly = document.getElementsByClassName("west-philly")[0];
+var centerCity = document.getElementsByClassName("center-city")[0];
+var southPhilly = document.getElementsByClassName("south-philly")[0];
+var northPhilly = document.getElementsByClassName("north-philly")[0];
+var northernLiberties = document.getElementsByClassName("northern-liberties")[0];
 var searchText;
+var i = 0;
+var searchLocation = "";
 
 submit.addEventListener("click", function(){
+    if (searchLocation = ""){
+        alert("Please click a neighborhood to search");
+    }
     searchText = search.value;
     searchYelp(searchText);
+});
+westPhilly.addEventListener("click", function(){
+    searchLocation = "west philly";
+});
+centerCity.addEventListener("click", function(){
+    searchLocation = "center city";
+});
+southPhilly.addEventListener("click", function(){
+    searchLocation = "south philly";
+});
+northPhilly.addEventListener("click", function(){
+    searchLocation = "north philly";
+});
+northernLiberties.addEventListener("click", function(){
+    searchLocation = "northern liberties";
 });
 
 function searchYelp(input){
@@ -61,13 +44,16 @@ function searchYelp(input){
         method: "GET",
         data: {
             term: searchText,
-            location: "west philadelphia",
+            location: searchLocation,
             radius: 1000,
             sort_by: "rating"
 
         },
         success: function(response){
             console.log(response);
+
+            i = 0;
+            results.innerHTML = "";
             listResults(response);
         }
 
@@ -76,7 +62,8 @@ function searchYelp(input){
 
 function listResults(inputObject){
     inputObject.businesses.forEach(function(result){
-        results.innerHTML += "<div class='result'><h2>" + result.name + "</h2><h3>Rating:" +result.rating + "</h3><h4>Phone: " + result.display_phone + "</h4>";
+        results.innerHTML += "<div class='result result" + i + "'><h2><a href='" + result.url + "'>" + result.name + "</a></h2><h3>Rating: " + result.rating + "</h3><h4>Phone: " + result.display_phone + "</h4></a>";
+        i += 1;
     });
 }
 
